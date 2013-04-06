d86fd57a96fb85c7f8f7a6d6d86424d6f479f744
Chantal Ding <chantal.ding_github@m4x.org>
2013-04-06 23:09:34 +0200

    Testing Data class

--- src/Test.java
+++ src/Test.java
@@ -1,7 +1,15 @@
+import java.io.IOException;
+
 
 public class Test {
-	public static void main (String[] args) {
+	public static void main (String[] args) throws IOException, EndFileException {
+		String name = "G:/Documents/X/Scola/2012_2013/INF431/projet/test.txt";
+		Data shakespeare = new Data(name,Data.FILE);
+		for (int i = 0;i<=50;i++){
+			System.out.println(shakespeare.nextElement().GetContent());
+		}
 		System.out.printf("aaa");
-		new Element("aaa");
 	}
 }
+
+
