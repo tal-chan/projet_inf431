b820c0b04ad324735384452e87ace358b5ee2132
Chantal Ding <chantal.ding_github@m4x.org>
2013-04-14 22:21:35 +0200

    accendted letters in isLetter

--- src/Word.java
+++ src/Word.java
@@ -1,9 +1,6 @@
-/*
- * Defines whether or not a char represents a letter
- * in the broad sense (i.e : as opposed to a separator)
- */
 public class Word {
+	
 	public static boolean isLetter(char c){
-		return (c>='0'&&c<='9')||(c>='a'&&c<='z')||(c>='A'&&c<='Z')||c=='-';
+		return (c>='0'&&c<='9')||(c>='a'&&c<='z')||(c>='A'&&c<='Z')||c=='-'||(c>=128&&c<=154)||(c>=160&&c<=165);
 	}
 }
