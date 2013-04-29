5d81469f009979f7d876b5bdae4f7bff0a143947
Chantal Ding <chantal.ding_github@m4x.org>
2013-04-29 18:23:35 +0200

    HashTest

--- src/HashTest.java
+++ src/HashTest.java
@@ -1,13 +1,24 @@
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
 
 public class HashTest {
 
 	/*
-	 *  
+	 *  In construction...
 	 */
-	
-	public static void main(String[] args) {
+
+	public static void main(String[] args) throws IOException {
 		String name = "C:/Users/Chantal Ding/Dropbox/INF431_Projet_chaton/Complete.txt";
+		Data data = new Data(name,Data.FILE);
+		Map <Integer,Integer> count = new HashMap<Integer,Integer>();
+		try {
+			for(Element el = data.FirstElement(1);;el=data.nextElement()) {
+				int hash = el.GetHash();
+			}
 
+		} catch (Data.NoMoreElement e) {}
+		
 	}
-
 }
